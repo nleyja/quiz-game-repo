747bb19c4c2d2cc7e8e722ed4ae3b2f71c2104fa
Noah <nleyja@gmail.com>
2022-08-08 09:13:18 -0500

    added reset/refresh button

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // reset or reload the game with button to start over. 
 
 
-const buttonPress = document.querySelector(".button");
+const buttonPress = document.querySelector(".button-grid");
 const containerChange = document.querySelector(".question-container");
 const rightAnswerButton = document.querySelectorAll(".right-answer");
 const wrongAnswerButton = document.querySelectorAll(".wrong-answer");
@@ -16,11 +16,8 @@ const handleCorrectAnswerPress = (event) => {
     containerChange.style.backgroundColor = "green";
     buttonPress.style.backgroundColor = "green";
   
-  
   // event.target.style.backgroundColor = "green"
   // document.body.style.backgroundColor = "green"
-
-   
 };
 
 for (let i = 0; i < rightAnswerButton.length; i++) {
@@ -31,11 +28,9 @@ const handleWrongAnswerPress = (event) => {
   containerChange.style.backgroundColor = "red";
   buttonPress.style.backgroundColor = "red";
 
-
 // event.target.style.backgroundColor = "green"
 // document.body.style.backgroundColor = "green"
 
- 
 };
 
 for (let i = 0; i < wrongAnswerButton.length; i++) {
