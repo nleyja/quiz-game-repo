4c972719ef91e27afb3072cdf52e7967fa317cab
Noah <nleyja@gmail.com>
2022-08-08 17:10:22 -0500

    added more questions

--- script.js
+++ script.js
@@ -7,32 +7,152 @@
 // reset or reload the game with button to start over. 
 
 
-const buttonPress = document.querySelector(".button-grid");
+const bgButtonColor1 = document.querySelector(".button-grid");
+const bgButtonColor2 = document.querySelector(".button-grid-two");
+const bgButtonColor3 = document.querySelector(".button-grid-three");
+const bgButtonColor4 = document.querySelector(".button-grid-four");
+const bgButtonColor5 = document.querySelector(".button-grid-five");
 const containerChange = document.querySelector(".question-container");
-const rightAnswerButton = document.querySelectorAll(".right-answer");
-const wrongAnswerButton = document.querySelectorAll(".wrong-answer");
+const containerChangeTwo = document.querySelector(".question-container-two");
+const containerChangeThree = document.querySelector(".question-container-three");
+const containerChangeFour = document.querySelector(".question-container-four");
+const containerChangeFive = document.querySelector(".question-container-five");
+const rightAnswerButtonOne = document.querySelector(".right-answer-one");
+const wrongAnswerButtonOne = document.querySelectorAll(".wrong-answer-one");
+const rightAnswerButtonTwo = document.querySelector(".right-answer-two");
+const wrongAnswerButtonTwo = document.querySelectorAll(".wrong-answer-two");
+const rightAnswerButtonThree = document.querySelector(".right-answer-three");
+const wrongAnswerButtonThree = document.querySelectorAll(".wrong-answer-three");
+const rightAnswerButtonFour = document.querySelector(".right-answer-four");
+const wrongAnswerButtonFour = document.querySelectorAll(".wrong-answer-four");
+const rightAnswerButtonFive = document.querySelector(".right-answer-five");
+const wrongAnswerButtonFive = document.querySelectorAll(".wrong-answer-five");
 
 const handleCorrectAnswerPress = (event) => {
     containerChange.style.backgroundColor = "green";
-    buttonPress.style.backgroundColor = "green";
+    bgButtonColor1.style.backgroundColor = "green";
+    
   
   // event.target.style.backgroundColor = "green"
   // document.body.style.backgroundColor = "green"
 };
 
-for (let i = 0; i < rightAnswerButton.length; i++) {
-  rightAnswerButton[i].addEventListener("click", handleCorrectAnswerPress);
-};
+rightAnswerButtonOne.addEventListener("click", handleCorrectAnswerPress);
+  
 
 const handleWrongAnswerPress = (event) => {
   containerChange.style.backgroundColor = "red";
-  buttonPress.style.backgroundColor = "red";
+  bgButtonColor1.style.backgroundColor = "red";
 
-// event.target.style.backgroundColor = "green"
+//event.target.style.backgroundColor = "green"
 // document.body.style.backgroundColor = "green"
 
 };
 
-for (let i = 0; i < wrongAnswerButton.length; i++) {
-wrongAnswerButton[i].addEventListener("click", handleWrongAnswerPress);
+for (let i = 0; i < wrongAnswerButtonOne.length; i++) {
+wrongAnswerButtonOne[i].addEventListener("click", handleWrongAnswerPress);
+};
+
+
+
+const handleCorrectAnswerPressTwo = (event) => {
+  containerChangeTwo.style.backgroundColor = "green";
+  bgButtonColor2.style.backgroundColor = "green";
+
+// event.target.style.backgroundColor = "green"
+// document.body.style.backgroundColor = "green"
+};
+
+rightAnswerButtonTwo.addEventListener("click", handleCorrectAnswerPressTwo);
+
+
+const handleWrongAnswerPressTwo = (event) => {
+containerChangeTwo.style.backgroundColor = "red";
+bgButtonColor2.style.backgroundColor = "red";
+
+//  event.target.style.backgroundColor = "green"
+// document.body.style.backgroundColor = "green"
+
+};
+
+for (let i = 0; i < wrongAnswerButtonTwo.length; i++) {
+wrongAnswerButtonTwo[i].addEventListener("click", handleWrongAnswerPressTwo);
+};
+
+const handleCorrectAnswerPressThree = (event) => {
+  containerChangeThree.style.backgroundColor = "green";
+  bgButtonColor3.style.backgroundColor = "green";
+
+// event.target.style.backgroundColor = "green"
+// document.body.style.backgroundColor = "green"
+};
+
+rightAnswerButtonThree.addEventListener("click", handleCorrectAnswerPressThree);
+
+
+const handleWrongAnswerPressThree = (event) => {
+containerChangeThree.style.backgroundColor = "red";
+bgButtonColor3.style.backgroundColor = "red";
+
+//  event.target.style.backgroundColor = "green"
+// document.body.style.backgroundColor = "green"
+
+};
+
+for (let i = 0; i < wrongAnswerButtonThree.length; i++) {
+wrongAnswerButtonThree[i].addEventListener("click", handleWrongAnswerPressThree);
+};
+
+
+
+
+const handleCorrectAnswerPressFour = (event) => {
+  containerChangeFour.style.backgroundColor = "green";
+  bgButtonColor4.style.backgroundColor = "green";
+
+// event.target.style.backgroundColor = "green"
+// document.body.style.backgroundColor = "green"
+};
+
+rightAnswerButtonFour.addEventListener("click", handleCorrectAnswerPressFour);
+
+
+const handleWrongAnswerPressFour = (event) => {
+containerChangeFour.style.backgroundColor = "red";
+bgButtonColor4.style.backgroundColor = "red";
+
+//  event.target.style.backgroundColor = "green"
+// document.body.style.backgroundColor = "green"
+
+};
+
+for (let i = 0; i < wrongAnswerButtonFour.length; i++) {
+wrongAnswerButtonFour[i].addEventListener("click", handleWrongAnswerPressFour);
+};
+
+
+
+
+const handleCorrectAnswerPressFive = (event) => {
+  containerChangeFive.style.backgroundColor = "green";
+  bgButtonColor5.style.backgroundColor = "green";
+
+// event.target.style.backgroundColor = "green"
+// document.body.style.backgroundColor = "green"
+};
+
+rightAnswerButtonFive.addEventListener("click", handleCorrectAnswerPressFive);
+
+
+const handleWrongAnswerPressFive = (event) => {
+containerChangeFive.style.backgroundColor = "red";
+bgButtonColor5.style.backgroundColor = "red";
+
+//  event.target.style.backgroundColor = "green"
+// document.body.style.backgroundColor = "green"
+
+};
+
+for (let i = 0; i < wrongAnswerButtonThree.length; i++) {
+wrongAnswerButtonFive[i].addEventListener("click", handleWrongAnswerPressFive);
 };
